bc804bfff0f0f574e39c94fdca1f0847efa27d9d
devstefancho <skku7714@gmail.com>
2024-01-03 23:37:25 +0900

    chore: rename title

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import starlight from '@astrojs/starlight'
 export default defineConfig({
   integrations: [
     starlight({
-      title: 'My Docs',
+      title: 'Vim Korea',
       social: {
         github: 'https://github.com/vim-kr/renewal',
       },
