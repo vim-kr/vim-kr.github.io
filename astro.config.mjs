a7c6185205a1a642c23d94cf567813a65077dd07
malkoG <rijgndqw012@gmail.com>
2024-01-04 00:15:56 +0900

    feat: Add site integration in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import starlight from '@astrojs/starlight'
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://next.vim.kr',
   integrations: [
     starlight({
       title: 'Vim Korea',
